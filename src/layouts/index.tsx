923c65dde956700c2bc6924d6272e3dc2e5c9b0b
vander <vanderdeng@gmail.com>
2022-09-15 15:00:50 +1000

    docs: delete useless layout&components

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { Box, Flex } from "@chakra-ui/react";
 import Footer from "./Footer";
-import ThemeToggle from "../components/ThemeToggle";
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,7 +11,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <Flex as="header" width="full" align="center">
         <Box marginLeft="auto" marginTop={5} marginRight={5}>
-          <ThemeToggle />
         </Box>
       </Flex>
       <Box as="main">{children}</Box>
