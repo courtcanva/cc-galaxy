5b88886145f0cb134cf7d677e541ddb9ac8896ff
vander <vanderdeng@gmail.com>
2022-09-15 19:32:38 +1000

    revert: style theme structure revert

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,10 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
+import customTheme from "./theme";
 
 interface ChakraProps {
   children: React.ReactNode;
 }
 
 export const Chakra = ({ children }: ChakraProps) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return <ChakraProvider theme={customTheme}>{children}</ChakraProvider>;
 };
