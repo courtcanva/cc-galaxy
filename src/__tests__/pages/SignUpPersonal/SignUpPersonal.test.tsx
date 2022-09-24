fed3aa418bbfa7e1409987f2304f7d8a1743cc11
Ethan <ethan@yaoni.me>
2022-09-24 15:27:08 +1000

    Add test to test onChange function.

--- src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
+++ src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderWithMockedProvider from "../../testHelper";
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import SignUpPersonal from "../../../pages/signUpPersonal";
 import { FormActionKind, initialFormState } from "@/components/SignupPersonal/SignUpReducer";
 import formReducer from "@/components/SignupPersonal/SignUpReducer";
@@ -16,7 +16,7 @@ describe("Personal Sign Up Page", () => {
   });
 });
 
-describe(" Personal Sign Up Reducer", () => {
+describe("Personal Sign Up Reducer", () => {
   it("should return empty", () => {
     const startAction = { type: FormActionKind.HANDLE_SIGNUP_INPUT };
     expect(formReducer({}, startAction)).toEqual({});
@@ -24,5 +24,19 @@ describe(" Personal Sign Up Reducer", () => {
   it("should match original state", () => {
     const startAction = { type: "RANDOM" };
     expect(formReducer(initialFormState, startAction)).toEqual(initialFormState);
+  });
+});
+
+describe("Testing onChange functions", () => {
+  it("should update text", () => {
+    //Rendering the component and its tree
+    renderWithMockedProvider(<SignUpPersonal />);
+    //Extracting the child, username_input component with his accessibilityLabel
+    // const usernameInput = screen.getByLabelText("password");
+    const emailInput = screen.getByLabelText("email");
+    //Fire a native changeText event with a specific value
+    fireEvent.change(emailInput, { target: { value: "jane@doe.com" } });
+    //Checking the rendered value
+    expect(emailInput.value).toBe("jane@doe.com");
   });
 });
