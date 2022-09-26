b102306d18daac068d4ba5dfdd9361cfaee19202
vander <vanderdeng@gmail.com>
2022-09-26 18:06:14 +1000

    fix: delete token

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import store from "../store";
 import { Chakra } from "@/styles/index";
 import Layout from "@/layouts";
-import Login from "./login";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 
@@ -22,17 +21,6 @@ type AppPropsWithLayout = AppProps & {
 };
 
 function CourtCanvaApp({ Component, pageProps }: AppPropsWithLayout) {
-  let accessToken: string | undefined;
-
-  if (typeof accessToken !== "string")
-    return (
-      <Chakra>
-        <Provider store={store}>
-          <Login />
-        </Provider>
-      </Chakra>
-    );
-
   return (
     <Chakra>
       <Provider store={store}>
