269f1c9bdabce1ea99bbeaddcdaf3b084d1287d7
Yaoni <y.wang7@uq.net.au>
2022-09-27 22:51:40 +1000

    Update test to match code changes.

--- src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
+++ src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
@@ -8,11 +8,11 @@ import formReducer from "@/components/SignupPersonal/SignUpReducer";
 describe("Personal Sign Up Page", () => {
   it("should render personal signup page success", () => {
     renderWithMockedProvider(<SignUpPersonal />);
-    expect(screen.getByText("residentialAddress")).toBeInTheDocument();
+    expect(screen.getByText("Residential Address")).toBeInTheDocument();
   });
   test("should render personal signup page success", () => {
     renderWithMockedProvider(<SignUpPersonal />);
-    expect(screen.getByText("residentialAddress")).toBeInTheDocument();
+    expect(screen.getByText("Residential Address")).toBeInTheDocument();
   });
 });
 
@@ -32,11 +32,25 @@ describe("Testing onChange functions", () => {
     //Rendering the component and its tree
     renderWithMockedProvider(<SignUpPersonal />);
     //Extracting the child, username_input component with his accessibilityLabel
-    // const usernameInput = screen.getByLabelText("password");
-    const emailInput = screen.getByLabelText("email");
+    const firstNameInput = screen.getByLabelText("First Name");
+    const lastNameInput = screen.getByLabelText("Last Name");
+    const phoneNumberInput = screen.getByLabelText("Phone number");
+    const residentialAddressInput = screen.getByLabelText("Residential Address");
+    const postcodeInput = screen.getByLabelText("Postcode");
+    const stateInput = screen.getByLabelText("State");
     //Fire a native changeText event with a specific value
-    fireEvent.change(emailInput, { target: { value: "jane@doe.com" } });
+    fireEvent.change(firstNameInput, { target: { value: "Jane" } });
+    fireEvent.change(lastNameInput, { target: { value: "Doe" } });
+    fireEvent.change(phoneNumberInput, { target: { value: "0439493939" } });
+    fireEvent.change(residentialAddressInput, { target: { value: "test" } });
+    fireEvent.change(postcodeInput, { target: { value: "test" } });
+    fireEvent.change(stateInput, { target: { value: "QLD" } });
     //Checking the rendered value
-    expect(emailInput.value).toBe("jane@doe.com");
+    expect(firstNameInput.value).toBe("Jane");
+    expect(phoneNumberInput.value).toBe("0439493939");
+    expect(residentialAddressInput.value).toBe("test");
+    expect(postcodeInput.value).toBe("test");
+    // FIXME: this is not right
+    expect(stateInput.value).toBe("QLD");
   });
 });
