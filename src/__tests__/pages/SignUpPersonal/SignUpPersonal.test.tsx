009a50f863d119e08769621b21f868e61f3fcd7c
Ethan <ethan@yaoni.me>
2022-09-28 16:10:53 +1000

    Remove SignUpAction logic.

--- src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
+++ src/__tests__/pages/SignUpPersonal/SignUpPersonal.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import renderWithMockedProvider from "../../testHelper";
-import { fireEvent, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import SignUpPersonal from "../../../pages/sign-up";
-import { FormActionKind, initialFormState } from "@/components/SignupPersonal/SignUpReducer";
-import formReducer from "@/components/SignupPersonal/SignUpReducer";
+import SignUpForm from "@/components/SignupPersonal/SignUpForm";
 
 describe("Personal Sign Up Page", () => {
   it("should render personal signup page success", () => {
@@ -13,17 +13,6 @@ describe("Personal Sign Up Page", () => {
   test("should render personal signup page success", () => {
     renderWithMockedProvider(<SignUpPersonal />);
     expect(screen.getByText("Residential Address")).toBeInTheDocument();
-  });
-});
-
-describe("Personal Sign Up Reducer", () => {
-  it("should return empty", () => {
-    const startAction = { type: FormActionKind.HANDLE_SIGNUP_INPUT };
-    expect(formReducer({}, startAction)).toEqual({});
-  });
-  it("should match original state", () => {
-    const startAction = { type: "RANDOM" };
-    expect(formReducer(initialFormState, startAction)).toEqual(initialFormState);
   });
 });
 
@@ -52,5 +41,47 @@ describe("Testing onChange functions", () => {
     expect(postcodeInput.value).toBe("test");
     // FIXME: this is not right
     expect(stateInput.value).toBe("QLD");
+  });
+});
+
+describe("Testing Sign Up Form", () => {
+  it("should render form", () => {
+    renderWithMockedProvider(<SignUpForm isLoggedIn={false} />);
+    expect(screen.getByText("Residential Address")).toBeInTheDocument();
+  });
+  it("should render prompt", () => {
+    renderWithMockedProvider(<SignUpForm isLoggedIn={true} />);
+    expect(screen.getByText("You have already logged in")).toBeInTheDocument();
+  });
+});
+
+describe("Testing form validation", () => {
+  it("should show warning", async () => {
+    //Rendering the component and its tree
+    renderWithMockedProvider(<SignUpPersonal />);
+    //Extracting the child, username_input component with his accessibilityLabel
+    const firstNameInput = screen.getByLabelText("First Name");
+    const lastNameInput = screen.getByLabelText("Last Name");
+    const phoneNumberInput = screen.getByLabelText("Phone number");
+    const residentialAddressInput = screen.getByLabelText("Residential Address");
+    const postcodeInput = screen.getByLabelText("Postcode");
+    const stateInput = screen.getByLabelText("State");
+    const submitBtn = screen.getByText("Sign up");
+    //Fire a native changeText event with a specific value
+    fireEvent.change(firstNameInput, { target: { value: "Jane" } });
+    fireEvent.change(lastNameInput, { target: { value: "Doe" } });
+    fireEvent.change(phoneNumberInput, { target: { value: "0439493939" } });
+    fireEvent.change(residentialAddressInput, { target: { value: "test" } });
+    fireEvent.change(postcodeInput, { target: { value: "test" } });
+    fireEvent.change(stateInput, { target: { value: "QLD" } });
+    await waitFor(() => user.click(submitBtn));
+
+    //Checking the rendered value
+    expect(screen.getByText("String must contain at least 1 character(s)")).toBeInTheDocument();
+    expect("The Phone Number does not match required format").toBeInTheDocument();
+    expect("The Postcode does not match the required format").toBeInTheDocument();
+    expect(
+      "Invalid enum value. Expected 'QLD' | 'VIC' | 'NSW' | 'NT' | 'SA' | 'ACT' | 'TAS' | 'WA', received ''"
+    ).toBeInTheDocument();
   });
 });
