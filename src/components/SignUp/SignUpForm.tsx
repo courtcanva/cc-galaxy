b9a0a3080a89b7325028bbab9629cab7d652fbf4
Prom1s1ngYoung <yqin7516@gmail.com>
2022-09-28 23:02:37 +1000

    message

--- src/components/SignUp/SignUpForm.tsx
+++ src/components/SignUp/SignUpForm.tsx
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
 import HeaderLayout from "../../layouts/HeaderLayout";
-import { 
-  Box, 
-  Tabs, 
-  TabPanels, 
-  TabPanel, 
-  Button, 
+import {
+  Box,
+  Tabs,
+  TabPanels,
+  TabPanel,
+  Button,
   Flex,
   Slider,
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
   SliderMark,
-  Stack} from "@chakra-ui/react";
+  Stack,
+} from "@chakra-ui/react";
 import SignUpFirstStep from "./SignUpFirstStep";
 
 const SignUpForm = () => {
   const [tabIndex, setTabIndex] = useState(0);
   const [isDisabled, setIsDisabled] = useState(true);
   const labelStyles = {
-      mt: '-3.5',
-      ml: '-2.5',
-      fontSize: 'sm',
-    }
+    mt: "-3.5",
+    ml: "-2.5",
+    fontSize: "sm",
+  };
 
   // const handleTabsChange = (index: number) => {
   //   setTabIndex(index);
   // };
 
   const handleNextButton = () => {
-    if (tabIndex < 2){
-      setTabIndex(tabIndex+1);
+    if (tabIndex < 2) {
+      setTabIndex(tabIndex + 1);
     }
   };
 
   const handleBackButton = () => {
-    setTabIndex(tabIndex-1);
+    setTabIndex(tabIndex - 1);
   };
 
   return (
@@ -56,8 +57,16 @@ const SignUpForm = () => {
             </TabPanel>
           </TabPanels>
         </Tabs>
-        <Flex justifyContent="center" alignItems="center" direction="column" >
-          <Slider aria-label='slider-ex-6' value={tabIndex} min={0} max={2} step={1} isDisabled= {true} height={45}>
+        <Flex justifyContent="center" alignItems="center" direction="column">
+          <Slider
+            aria-label="slider-ex-6"
+            value={tabIndex}
+            min={0}
+            max={2}
+            step={1}
+            isDisabled={true}
+            height={45}
+          >
             <SliderMark value={0.03} {...labelStyles}>
               Step 1
             </SliderMark>
@@ -72,24 +81,21 @@ const SignUpForm = () => {
             </SliderTrack>
             <SliderThumb />
           </Slider>
-          <Stack direction='row' spacing={4} align='center'>
+          <Stack direction="row" spacing={4} align="center">
             {tabIndex === 0 ? (
-            <Button
-              colorScheme="green"
-              disabled={isDisabled}
-              onClick={handleNextButton}
-            >Next
-            </Button>) : (
-            <><Button 
-            colorScheme="green"
-            onClick={handleBackButton}
-            >Back</Button>
-            <Button
-            colorScheme="gray"
-            disabled={isDisabled}
-            onClick={handleNextButton}
-            >Next
-            </Button></>) }
+              <Button colorScheme="green" disabled={isDisabled} onClick={handleNextButton}>
+                Next
+              </Button>
+            ) : (
+              <>
+                <Button colorScheme="green" onClick={handleBackButton}>
+                  Back
+                </Button>
+                <Button colorScheme="gray" disabled={isDisabled} onClick={handleNextButton}>
+                  Next
+                </Button>
+              </>
+            )}
           </Stack>
         </Flex>
       </Box>
