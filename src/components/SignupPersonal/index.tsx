ed24e87e99fd6684b7964906320e6e26f58d8938
Ethan <ethan@yaoni.me>
2022-09-29 21:14:08 +1000

    fix: improve sign-up form UI

--- src/components/SignupPersonal/index.tsx
+++ src/components/SignupPersonal/index.tsx
@@ -6,15 +6,9 @@ import { Box, Flex, Image, Text } from "@chakra-ui/react";
 const SignUp: React.FC = () => {
   return (
     <HeaderLayout>
-      <Flex width="100vw" height="100vh" alignItems="center" background="#114458">
+      <Flex alignItems="center" background="#114458">
         <Flex justifyContent="center" alignItems="center" position="relative">
-          <Box
-            width="1280px"
-            height="710px"
-            background="background.secondary"
-            position="absolute"
-            transform="rotate(-3deg)"
-          />
+          <Box background="background.secondary" position="absolute" transform="rotate(-3deg)" />
 
           <Image
             src="./login/login-image.png"
@@ -24,22 +18,20 @@ const SignUp: React.FC = () => {
           />
 
           <Flex
-            width="629px"
-            height="682px"
             flexDir="column"
             alignItems="center"
-            justifyContent="space-around"
+            justifyContent="flex-start"
             background="white"
             zIndex="10"
           >
-            <Flex flexDir="column" alignItems="center" marginTop="50px">
+            <Flex flexDir="column" alignItems="center" justifyContent="flex-start" marginTop="50px">
               <Image boxSize="64px" alt="dashboard-logo-192x192" src="dashboard-logo-192x192.png" />
               <Text marginTop="10px" color="brand.secondary">
                 CourtCanva
               </Text>
             </Flex>
 
-            <Flex alignItems="flex-start">
+            <Flex>
               <Text>Please fill in your personal information</Text>
             </Flex>
 
