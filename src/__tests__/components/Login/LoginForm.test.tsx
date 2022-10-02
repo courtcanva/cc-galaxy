707469f9fcb27c13f9fc0ff7acf55ee15b1402a6
vander <vanderdeng@gmail.com>
2022-10-02 11:55:29 +1000

    fix: debug axios mock test

--- src/__tests__/components/Login/LoginForm.test.tsx
+++ src/__tests__/components/Login/LoginForm.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import renderWithMockedProvider from "../../testHelper";
 import user from "@testing-library/user-event";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import Login from "../../../pages/login";
 import customAxios from "../../../utils/axios";
 import MockAdapter from "axios-mock-adapter";
